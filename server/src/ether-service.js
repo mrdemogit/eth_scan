890203371164830cec35d00d6ee398d73b692550
Maros Demko <maros.demko.it@gmail.com>
2019-02-17 09:19:19 +0800

    [Server] Move transformation logic to Client side

--- server/src/ether-service.js
+++ server/src/ether-service.js
@@ -2,7 +2,6 @@
 const fetch = require('node-fetch');
 const querystring = require('querystring');
 const { Observable } = require('rxjs');
-const { reduce, concatMap } = require('rxjs/operators');
 const config = require('./config');
 
 const formatParams = params =>
@@ -37,29 +36,11 @@ const fetchTransactions = params =>
   });
 
 /**
- * Converts items to object with transactions Map<hash,object> and Order[]
- * @type {[type]}
- */
-const reduceToHashMap = reduce((prevItem, newItem) => {
-  if (!prevItem.order) {
-    prevItem.order = [];
-  }
-  return {
-    transactionsMap: { ...prevItem.transactionsMap, [newItem.hash]: newItem },
-    order: [...prevItem.order, newItem.hash],
-  };
-}, {});
-
-/**
  * Search for transactions and transform to map
  * @param  {Object} params ETH Parameters to fetch transactions
  * @return {Promise<Observable>} Stream of transactions data
  */
-const search = async params =>
-  fetchTransactions(params).pipe(
-    concatMap(response => response.result),
-    reduceToHashMap,
-  );
+const search = async params => fetchTransactions(params);
 
 module.exports = {
   search,
